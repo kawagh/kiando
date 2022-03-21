44be0c04f21ea5520f996771222fd2ab723806e4
kawagh <kawagh@example.com>
2022-03-21 14:56:09 +0900

    defined initialState of board and display char

--- app/src/main/java/com/example/kiando/MainScreen.kt
+++ app/src/main/java/com/example/kiando/MainScreen.kt
@@ -21,34 +21,46 @@ fun MainScreen() {
 
 @Composable
 fun Board() {
+    val b = initialBoardState
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
     ) {
         repeat(9) { rowIndex ->
-            BoardRow(rowIndex)
+            BoardRow(rowIndex, b[rowIndex])
         }
     }
 }
 
 @Composable
-fun BoardRow(rowIndex: Int) {
+fun BoardRow(rowIndex: Int, row: List<Piece>) {
     Row() {
         repeat(9) { colIndex ->
-            Panel(rowIndex * 9 + colIndex)
+            Panel(rowIndex * 9 + colIndex, row[colIndex])
 
         }
     }
 }
 
 @Composable
-fun Panel(state: Int) {
+fun Panel(state: Int, piece: Piece) {
     val context = LocalContext.current
     Button(
         onClick = { Toast.makeText(context, "$state clicked", Toast.LENGTH_SHORT).show() },
         modifier = Modifier.size(40.dp)
     ) {
-        Text(text = state.toString(), fontSize = 10.sp)
+        val text = when (piece) {
+            Piece.KING -> "王"
+            Piece.ROOK -> "飛"
+            Piece.BISHOP -> "角"
+            Piece.GOLD -> "金"
+            Piece.SILVER -> "銀"
+            Piece.KNIGHT -> "桂"
+            Piece.LANCE -> "香"
+            Piece.PAWN -> "歩"
+            Piece.EMPTY -> ""
+        }
+        Text(text = text, fontSize = 15.sp)
     }
 }
