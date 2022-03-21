a04bcb392d5608b1e3683e4ebc54ba1bf3832e6d
kawagh <kawagh@example.com>
2022-03-21 14:21:05 +0900

    show toast on panel click

--- app/src/main/java/com/example/kiando/MainScreen.kt
+++ app/src/main/java/com/example/kiando/MainScreen.kt
@@ -1,12 +1,16 @@
 package com.example.kiando
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 
 @Preview
@@ -22,18 +26,29 @@ fun Board() {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
     ) {
-        repeat(9) {
-            BoardRow()
+        repeat(9) { rowIndex ->
+            BoardRow(rowIndex)
         }
     }
 }
 
 @Composable
-fun BoardRow() {
+fun BoardRow(rowIndex: Int) {
     Row() {
-        repeat(9) {
-            Button(onClick = { /*TODO*/ }, modifier = Modifier.size(40.dp)) {
-            }
+        repeat(9) { colIndex ->
+            Panel(rowIndex * 9 + colIndex)
+
         }
+    }
+}
+
+@Composable
+fun Panel(state: Int) {
+    val context = LocalContext.current
+    Button(
+        onClick = { Toast.makeText(context, "$state clicked", Toast.LENGTH_SHORT).show() },
+        modifier = Modifier.size(40.dp)
+    ) {
+        Text(text = state.toString(), fontSize = 10.sp)
     }
 }
