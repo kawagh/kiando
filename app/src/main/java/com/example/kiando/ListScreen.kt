98d9991c884af46f5e8a4e0e4392f20a9158de58
kawagh <kawagh@example.com>
2022-03-22 13:40:25 +0900

    added sampleQuestions

--- app/src/main/java/com/example/kiando/ListScreen.kt
+++ app/src/main/java/com/example/kiando/ListScreen.kt
@@ -1,16 +1,29 @@
 package com.example.kiando
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun PreviewListScreen() {
+    ListScreen { }
+
+}
 
 @Composable
 fun ListScreen(onNavigateMain: () -> Unit) {
+    val questions = sampleQuestions
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -20,7 +33,32 @@ fun ListScreen(onNavigateMain: () -> Unit) {
         Button(onClick = onNavigateMain) {
             Text(text = "List")
         }
+        QuestionsList(questions = questions)
 
+    }
+}
+
+@Composable
+fun QuestionsList(questions: List<Question>) {
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+        items(questions) { question ->
+            QuestionRow(question)
+        }
+    }
+}
+
+@Composable
+fun QuestionRow(question: Question) {
+    Row() {
+        Text(text = question.description)
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "solve")
+        }
     }
 }
 
--- app/src/main/java/com/example/kiando/Question.kt
+++ app/src/main/java/com/example/kiando/Question.kt
@@ -7,3 +7,11 @@ val sampleQuestion = Question(
     boardState = initialBoardState, description = "角道を開ける手は?",
     answerMove = Move(Position(6, 2), Position(5, 2)),
 )
+val sampleQuestion2 = Question(
+    boardState = initialBoardState, description = "飛車先を突く手は?",
+    answerMove = Move(Position(6, 7), Position(5, 7)),
+)
+val sampleQuestions: List<Question> = listOf(
+    sampleQuestion,
+    sampleQuestion2,
+)
