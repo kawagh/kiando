c304d0060bef73ba509daca15cd9a4930528e1e8
kawagh <kawagh@example.com>
2022-03-24 14:33:51 +0900

    defined Question Entity

--- app/src/main/java/com/example/kiando/Question.kt
+++ app/src/main/java/com/example/kiando/Question.kt
@@ -1,27 +1,42 @@
 package com.example.kiando
 
+import androidx.room.Entity
+import androidx.room.Ignore
+import androidx.room.PrimaryKey
 
+
+@Entity(tableName = "questions")
 data class Question(
-    val boardState: BoardStateFlatten,
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    @Ignore val boardState: BoardStateFlatten,
     val description: String,
-    val answerMove: Move
+    val answerMove: Move,
+    val sfen: String,
 )
+
+val initialSFEN = "lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL"
+val sample3_SFEN = "lnsgkgsnl/1+r5b1/pppppp1pp/6p2/9/2P6/PP1PPPPPP/1B5R1/LNSGKGSNL b - 1"
 
 val sampleQuestion = Question(
     boardState = initialBoardState.flatten(), description = "角道を開ける手は?",
     answerMove = Move(Position(6, 2), Position(5, 2)),
+    sfen = initialSFEN,
 )
 val sampleQuestion2 = Question(
     boardState = initialBoardState.flatten(), description = "飛車先を突く手は?",
     answerMove = Move(Position(6, 7), Position(5, 7)),
+    sfen = initialSFEN,
 )
-val sample3_SFEN = "lnsgkgsnl/1+r5b1/pppppp1pp/6p2/9/2P6/PP1PPPPPP/1B5R1/LNSGKGSNL b - 1"
+
 val sampleQuestion3 =
     Question(
-        SFENConverter().convertFrom(sample3_SFEN), "角交換を防ぐ手は?", Move(
+        boardState = SFENConverter().convertFrom(sample3_SFEN),
+        description = "角交換を防ぐ手は?",
+        answerMove = Move(
             Position(6, 3),
             Position(5, 3)
-        )
+        ),
+        sfen = sample3_SFEN,
     )
 val sampleQuestions: List<Question> = listOf(
     sampleQuestion,
