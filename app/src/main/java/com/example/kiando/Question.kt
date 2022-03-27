bb832bb3d32776891cd8514f7f7eca730dc7ae52
kawagh <kawagh@example.com>
2022-03-27 16:47:41 +0900

    fixed promotion in sample3

--- app/src/main/java/com/example/kiando/Question.kt
+++ app/src/main/java/com/example/kiando/Question.kt
@@ -52,7 +52,7 @@ data class Question(
 }
 
 val initialSFEN = "lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL"
-val sample3_SFEN = "lnsgkgsnl/1+r5b1/pppppp1pp/6p2/9/2P6/PP1PPPPPP/1B5R1/LNSGKGSNL b - 1"
+val sample3_SFEN = "lnsgkgsnl/1r5b1/pppppp1pp/6p2/9/2P6/PP1PPPPPP/1B5R1/LNSGKGSNL b - 1"
 
 val sampleQuestion = Question(
     id = -1,
