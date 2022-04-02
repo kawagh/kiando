e29165de0eadfbbf7c2b8f53b7cfa37719c25688
kawagh <kawagh@example.com>
2022-04-02 16:02:45 +0900

    [feat] enabled deleteById Query(with Experimental) #24 ; added Stable Composable(without Experimental)

--- app/src/main/java/com/example/kiando/ListScreen.kt
+++ app/src/main/java/com/example/kiando/ListScreen.kt
@@ -1,7 +1,9 @@
 package com.example.kiando
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -21,7 +23,7 @@ import com.example.kiando.ui.theme.BoardColor
 @Preview
 @Composable
 fun PreviewListScreen() {
-    ListScreen(sampleQuestions, {}, {})
+    ListScreen(sampleQuestions, {}, {}, {})
 
 }
 
@@ -29,7 +31,8 @@ fun PreviewListScreen() {
 fun ListScreen(
     questions: List<Question>,
     navigateToQuestion: (Question) -> Unit,
-    handleDeleteQuestions: () -> Unit
+    handleDeleteQuestions: () -> Unit,
+    handleDeleteAQuestion: (Question) -> Unit,
 ) {
     var showDeleteDialog by remember {
         mutableStateOf(false)
@@ -72,27 +75,66 @@ fun ListScreen(
                 verticalArrangement = Arrangement.Center,
             ) {
                 Text(text = "Problem Set", fontSize = MaterialTheme.typography.h4.fontSize)
-                QuestionsList(questions = questions, navigateToQuestion)
+                QuestionsList(
+                    questions = questions,
+                    navigateToQuestion = navigateToQuestion,
+                    handleDeleteAQuestion = handleDeleteAQuestion,
+                )
             }
         },
     )
 }
 
 @Composable
-fun QuestionsList(questions: List<Question>, navigateToQuestion: (Question) -> Unit) {
+fun QuestionsList(
+    questions: List<Question>,
+    navigateToQuestion: (Question) -> Unit,
+    handleDeleteAQuestion: (Question) -> Unit,
+) {
     LazyColumn {
         items(questions) { question ->
             QuestionRow(
                 question = question,
                 onClick = { navigateToQuestion(question) },
+                handleDeleteAQuestion = { handleDeleteAQuestion(question) },
             )
             Spacer(Modifier.size(5.dp))
         }
     }
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun QuestionRow(question: Question, onClick: () -> Unit) {
+fun QuestionRow(question: Question, onClick: () -> Unit, handleDeleteAQuestion: () -> Unit) {
+    var showDeleteButton by remember {
+        mutableStateOf(false)
+    }
+    Row(
+        modifier = Modifier
+            .width(300.dp)
+            .clip(shape = RoundedCornerShape(20.dp))
+            .combinedClickable(
+                onClick = onClick,
+                onLongClick = { showDeleteButton = !showDeleteButton },
+            )
+            .background(BoardColor),
+        horizontalArrangement = Arrangement.Center,
+    )
+    {
+        Text(text = question.description, fontSize = MaterialTheme.typography.h5.fontSize)
+        if (showDeleteButton) {
+            Button(onClick = {
+                handleDeleteAQuestion.invoke()
+                showDeleteButton = false
+            }) {
+                Text(text = "delete")
+            }
+        }
+    }
+}
+
+@Composable
+fun StableQuestionRow(question: Question, onClick: () -> Unit, handleDeleteAQuestion: () -> Unit) {
     Row(
         modifier = Modifier
             .width(300.dp)
@@ -105,4 +147,3 @@ fun QuestionRow(question: Question, onClick: () -> Unit) {
         Text(text = question.description, fontSize = MaterialTheme.typography.h5.fontSize)
     }
 }
-
