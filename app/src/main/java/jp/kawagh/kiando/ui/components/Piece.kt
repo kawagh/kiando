ceb61b6246a6e404ae447075896cb47140566872
kawagh <kawagh@example.com>
2022-04-15 00:11:36 +0900

    [style] added PieceColor

--- app/src/main/java/jp/kawagh/kiando/ui/components/Piece.kt
+++ app/src/main/java/jp/kawagh/kiando/ui/components/Piece.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import jp.kawagh.kiando.ui.theme.PieceColor
 
 @Composable
 fun Piece(
@@ -41,7 +42,7 @@ fun Piece(
                     lineTo(w / 4, h / 3)
                     close()
                 },
-                color = Color.White,
+                color = PieceColor,
             )
         }
         Text(text = text, fontSize = 17.sp, color = if (isPromoted) Color.Red else Color.Black)
