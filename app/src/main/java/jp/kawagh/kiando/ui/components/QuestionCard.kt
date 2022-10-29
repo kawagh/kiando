b89cb3b36dfd0aa07018e9a44cb8e6632569edb6
kawagh <kawagh.dev@gmail.com>
2022-10-29 12:50:43 +0900

    switched to M3 in QuestionCard

--- app/src/main/java/jp/kawagh/kiando/ui/components/QuestionCard.kt
+++ app/src/main/java/jp/kawagh/kiando/ui/components/QuestionCard.kt
@@ -1,20 +1,19 @@
 package jp.kawagh.kiando.ui.components
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Star
-import androidx.compose.runtime.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import jp.kawagh.kiando.Question
 import jp.kawagh.kiando.sampleQuestion
-import jp.kawagh.kiando.ui.theme.BoardColor
 
-@OptIn(ExperimentalMaterialApi::class)
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun QuestionCard(
     question: Question,
@@ -23,15 +22,11 @@ fun QuestionCard(
     handleFavoriteQuestion: () -> Unit,
 ) {
     val isFavorite = question.tag_id == 1
-    Card(
-        onClick = onClick,
-        backgroundColor = BoardColor,
-        modifier = Modifier.padding(4.dp),
-    ) {
+    Card(onClick = onClick) {
         Column() {
             Text(
                 text = question.description,
-                fontSize = MaterialTheme.typography.h5.fontSize,
+                fontSize = MaterialTheme.typography.titleLarge.fontSize,
                 modifier = Modifier.padding(4.dp)
             )
             Row(horizontalArrangement = Arrangement.End, modifier = Modifier.fillMaxWidth()) {
