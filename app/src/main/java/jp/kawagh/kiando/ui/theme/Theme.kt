a418ccfec2d353feb980c84cce4959604dd70f1d
kawagh <kawagh.dev@gmail.com>
2023-01-29 12:55:18 +0900

    removed material dependency

--- app/src/main/java/jp/kawagh/kiando/ui/theme/Theme.kt
+++ app/src/main/java/jp/kawagh/kiando/ui/theme/Theme.kt
@@ -1,33 +1,10 @@
 package jp.kawagh.kiando.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
-import androidx.compose.material.lightColors
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-
-private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
-)
-
-private val LightColorPalette = lightColors(
-    primary = BoardColorUnfocused,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
-)
 
 /*
     for Mateiral 3
@@ -45,30 +22,14 @@ private val LightColorScheme = lightColorScheme(
 
 
 @Composable
-fun KiandoTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
-
-    MaterialTheme(
-        colors = colors,
-        typography = Typography,
-        shapes = Shapes,
-        content = content
-    )
-}
-
-@Composable
 fun KiandoM3Theme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
     val colorScheme = if (darkTheme) {
         DarkColorScheme
     } else LightColorScheme
 
-    androidx.compose.material3.MaterialTheme(
+    MaterialTheme(
         colorScheme = colorScheme,
-        typography = TypographyM3,
+        typography = Typography,
         content = content
     )
 }
