c8715bbd9dd1199233438ad409678e9ae2f7c465
kawagh <kawagh.dev@gmail.com>
2023-01-29 13:11:01 +0900

    fixed build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,79 +1,84 @@
+@file:Suppress("UnstableApiUsage")
+
 plugins {
-    id 'com.android.application'
-    id 'org.jetbrains.kotlin.android'
-    id 'kotlin-kapt'
-    id "com.cookpad.android.plugin.license-tools" version "1.2.8"
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+    id("com.cookpad.android.plugin.license-tools") version "1.2.8"
 }
 
 android {
-    compileSdk 33
+    compileSdk = 33
     defaultConfig {
-        applicationId "jp.kawagh.kiando"
-        minSdk 21
-        targetSdk 33
-        versionCode 10005
-        versionName "1.0.5"
+        applicationId = "jp.kawagh.kiando"
+        minSdk = 21
+        targetSdk = 33
+        versionCode = 10005
+        versionName = "1.0.5"
 
-        testInstrumentationRunner "androidx.test.runner.AndroidJUnitRunner"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
-            useSupportLibrary true
+            useSupportLibrary = true
         }
     }
 
     buildTypes {
         release {
-            minifyEnabled false
-            proguardFiles getDefaultProguardFile('proguard-android-optimize.txt'), 'proguard-rules.pro'
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
     compileOptions {
-        sourceCompatibility JavaVersion.VERSION_1_8
-        targetCompatibility JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
     kotlinOptions {
-        jvmTarget = '1.8'
+        jvmTarget = "1.8"
     }
     buildFeatures {
-        compose true
+        compose = true
     }
     composeOptions {
         kotlinCompilerExtensionVersion = "1.3.2"
     }
     packagingOptions {
         resources {
-            excludes += '/META-INF/{AL2.0,LGPL2.1}'
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
 }
 
 dependencies {
+    implementation("androidx.core:core-ktx:1.9.0")
+    val compose_version: String by project
+    implementation("androidx.compose.ui:ui:$compose_version")
+    val material3_version = "1.0.1"
+    implementation("androidx.compose.material3:material3:$material3_version")
+    implementation("androidx.compose.ui:ui-tooling-preview:$compose_version")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
+    implementation("androidx.activity:activity-compose:1.6.1")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:$compose_version")
+    androidTestImplementation("androidx.test:core:1.5.0")
+    debugImplementation("androidx.compose.ui:ui-tooling:$compose_version")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:$compose_version")
 
-    implementation 'androidx.core:core-ktx:1.9.0'
-    implementation "androidx.compose.ui:ui:$compose_version"
-    def material3_version = "1.0.1"
-    implementation "androidx.compose.material3:material3:$material3_version"
-    implementation "androidx.compose.ui:ui-tooling-preview:$compose_version"
-    implementation 'androidx.lifecycle:lifecycle-runtime-ktx:2.5.1'
-    implementation 'androidx.activity:activity-compose:1.6.1'
-    testImplementation 'junit:junit:4.13.2'
-    androidTestImplementation 'androidx.test.ext:junit:1.1.5'
-    androidTestImplementation 'androidx.test.espresso:espresso-core:3.5.1'
-    androidTestImplementation "androidx.compose.ui:ui-test-junit4:$compose_version"
-    androidTestImplementation "androidx.test:core:1.5.0-rc01"
-    debugImplementation "androidx.compose.ui:ui-tooling:$compose_version"
-    debugImplementation "androidx.compose.ui:ui-test-manifest:$compose_version"
+    val nav_version = "2.5.2"
+    implementation("androidx.navigation:navigation-compose:$nav_version")
 
-    def nav_version = "2.5.2"
-    implementation "androidx.navigation:navigation-compose:$nav_version"
+    val room_version = "2.4.3"
+    implementation("androidx.room:room-runtime:$room_version")
+    implementation("androidx.room:room-ktx:$room_version")
+    kapt("androidx.room:room-compiler:$room_version")
+    implementation("androidx.compose.runtime:runtime-livedata:1.4.0-alpha04")
 
-    def room_version = "2.4.3"
-    implementation "androidx.room:room-runtime:$room_version"
-    implementation "androidx.room:room-ktx:$room_version"
-    kapt "androidx.room:room-compiler:$room_version"
-    implementation 'androidx.compose.runtime:runtime-livedata:1.4.0-alpha04'
-
-    implementation 'androidx.lifecycle:lifecycle-viewmodel-compose:2.5.1'
-    implementation "androidx.compose.material:material-icons-extended:$compose_version"
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.5.1")
+    implementation("androidx.compose.material:material-icons-extended:$compose_version")
     // change system-ui style
-    implementation "com.google.accompanist:accompanist-systemuicontroller:0.25.0"
+    implementation("com.google.accompanist:accompanist-systemuicontroller:0.25.0")
 }
