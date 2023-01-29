2807aa10f66a24055de72443a5a60324383da7bb
kawagh <kawagh.dev@gmail.com>
2023-01-29 12:48:03 +0900

    use m3 composable

--- app/src/main/java/jp/kawagh/kiando/ui/components/Komadai.kt
+++ app/src/main/java/jp/kawagh/kiando/ui/components/Komadai.kt
@@ -4,7 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Text
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
