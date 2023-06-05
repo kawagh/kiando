9a7083354e925a910c8c97ac68b24b6315621dd1
kawagh <kawagh.dev@gmail.com>
2023-06-05 22:15:46 +0900

    wip: add getSFENResponse()

--- app/src/main/java/jp/kawagh/kiando/network/KiandoApiService.kt
+++ app/src/main/java/jp/kawagh/kiando/network/KiandoApiService.kt
@@ -1,12 +1,21 @@
 package jp.kawagh.kiando.network
 
 import kotlinx.serialization.Serializable
+import okhttp3.RequestBody
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Multipart
+import retrofit2.http.POST
+import retrofit2.http.Part
 
 interface KiandoApiService {
     @GET("/")
     suspend fun getSFENResponse(): Response<SFENResponse>
+
+    // TODO set requestBody https://square.github.io/retrofit/
+    @Multipart
+    @POST("/")
+    suspend fun getSFENResponse(@Part("photo") photo: RequestBody): Response<SFENResponse>
 }
 
 @Serializable
