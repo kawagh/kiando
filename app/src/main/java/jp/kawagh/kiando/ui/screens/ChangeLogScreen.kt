b715cc6af878bc3b0869bb89dda9a9f4dea6b0da
kawagh <kawagh.dev@gmail.com>
2023-07-12 11:44:50 +0900

    update changelog in app

--- app/src/main/java/jp/kawagh/kiando/ui/screens/ChangeLogScreen.kt
+++ app/src/main/java/jp/kawagh/kiando/ui/screens/ChangeLogScreen.kt
@@ -35,6 +35,7 @@ object ChangeLogs {
     // want to manage only data or `CHANGELOG.md`
     val data: List<BaseChangeLog> =
         listOf(
+            ChangeLog(title = "SFEN入力フォーム以下の要素が重なる問題の修正", date = LocalDate.of(2023, 7, 12)),
             ChangeLog(title = "問題の解説の表示、編集機能の追加", date = LocalDate.of(2023, 7, 11)),
             ReleaseLog(version = "1.0.19", date = LocalDate.of(2023, 7, 7)),
             ChangeLog(title = "更新履歴の表示", date = LocalDate.of(2023, 7, 7)),
