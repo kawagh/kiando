f8c7f2d23fb1b2a56e878f0dcb7b31c9d0306bc2
kawagh <kawagh.dev@gmail.com>
2023-07-13 11:00:42 +0900

    update Makefile and picutres

--- shell/update_pictures.sh
+++ shell/update_pictures.sh
@@ -3,8 +3,10 @@
 device=$(adb devices | head -n 2 | tail -n 1 | tr '\t' ' ' | cut -d ' ' -f 1)
 application_id="jp.kawagh.kiando.debug"
 
+# pull feature graphics
 for i in $(seq 1 5); do
   adb -s "${device}" -d shell "run-as ${application_id} cat /data/user/0/${application_id}/files/feature_graphic${i}.png" > "pictures/feature_graphic${i}.png"
 done
 
+# pull app icon
 adb -s "${device}" -d shell "run-as ${application_id} cat /data/user/0/${application_id}/files/app_icon.png" > "pictures/app_icon.png"
