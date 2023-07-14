4bd5eb1b93566789c36b15ebc67e3bc9ec85d56e
kawagh <kawagh.dev@gmail.com>
2023-07-14 09:45:53 +0900

    parse releaselog

--- shell/generate_changelog.py
+++ shell/generate_changelog.py
@@ -22,21 +22,29 @@ def main():
     )
     assert changelogs_kt_file_path.exists(), "file not found"
 
-    texts = changelogs_kt_file_path.read_text()
-    # TODO parse releaselog
-    found = re.findall(r"\s.Change.*", texts)
-    for l in found:
-        # print(l)
-        if len(str(l).split('"')) <= 1:
-            continue
-        title = str(l).split('"')[1]
-        date = str(l).split(")")[-3].split("(")[-1]
-        yyyymmdd_tokens = date.split(",")
-        yyyymmdd = "".join(map(lambda x: str(int(x)).zfill(2), yyyymmdd_tokens))
-        changelog = ChangeLog(title, yyyymmdd)
-        print(changelog)
-
+    parsed_logs = []
+    for l in changelogs_kt_file_path.read_text().split("\n"):
+        if re.match(r"\s*Change.*", l):
+            if len(str(l).split('"')) <= 1:
+                continue
+            title = l.split('"')[1]
+            date = l.split(")")[-3].split("(")[-1]
+            yyyymmdd_tokens = date.split(",")
+            yyyymmdd = "".join(map(lambda x: str(int(x)).zfill(2), yyyymmdd_tokens))
+            changelog = ChangeLog(title, yyyymmdd)
+            parsed_logs.append(changelog)
+        if re.match(r"\s*ReleaseLog.*", l):
+            if len(str(l).split('"')) <= 1:
+                continue
+            version = l.split('"')[1]
+            date = l.split(")")[-3].split("(")[-1]
+            yyyymmdd_tokens = date.split(",")
+            yyyymmdd = "".join(map(lambda x: str(int(x)).zfill(2), yyyymmdd_tokens))
+            releaselog = ReleaseLog(version, yyyymmdd)
+            parsed_logs.append(releaselog)
+    print(*parsed_logs,sep="\n")
     # TODO generate CHANGELOG_.md
+
 
 if __name__ == "__main__":
     main()
