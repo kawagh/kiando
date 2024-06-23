1fcadb534d38d6abbee4bc1feb4c6834ab7d6c50
kawagh <kawagh.dev@gmail.com>
2024-06-23 10:15:14 +0900

    versioned v1.0.23

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,7 +24,7 @@ fun convertVersionNameToCode(versionName: String): Int {
     return 10000 * majorVersion + 100 * minorVersion + revision
 }
 
-val appVersion = "1.0.22"
+val appVersion = "1.0.23"
 
 android {
     compileSdk = 33
